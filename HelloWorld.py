a9b26b78fbfedf441aba1f46d4cd0b6c90ae8b53
Mauro Montaño <montanomauro369@gmail.com>
2024-01-01 09:30:26 -0300

    Dia_1

---
+++ HelloWorld.py
@@ -0,0 +1,20 @@
+#Practica dia 1
+print(2+5)
+print(2-5)
+print(2*5)
+print(2%5)
+print(2/5)
+print(2**5)
+print(5//2)
+
+
+#Write strings on the python interactive shell. The strings are the following:
+print("My nombre es Mauro")
+print("I am enjoying 30 days of python")
+
+#Check the data types of the following data:
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
